ff239f28b11b9f2d82e91165ede35f1e18d266a3
Matko Bulic <bulicmatko@gmail.com>
2020-12-19 03:25:34 +0100

    backup files before symlinking

--- install.sh
+++ install.sh
@@ -9,7 +9,9 @@ ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/the
 git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
 
 # Symlink oh-my-zsh
+mv ~./zshrc ~/.zshrc.backup
 ln -s ./.zshrc ~/.zshrc
 
 # Symlink gitconfig
+mv ~./gitconfig ~./gitconfig.backup
 ln -s ./.gitconfig ~/.gitconfig
